3854e8de9cd8971e3f4e0423f81412ac17627bb3
dlxiii <tokyowangyulong@gmail.com>
2022-06-18 14:32:30 +0900

    initial commit

---
+++ Msil.py
@@ -0,0 +1,13 @@
+"""
+API for fishery right data
+Code: Yulong Wang
+Date: Jun 17, 2022
+"""
+
+class Msil:
+    def __init__(self, url: str, where: where):
+        self.url
+        self.where
+        self.unit = 'meter'
+
+if __name__ == '__main__':
